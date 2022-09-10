fa8070aef07b3bf248b18865c7281cd074737896
大石头 <nnhy@vip.qq.com>
2022-09-10 09:13:22 +0800

    增加假期扩展

---
+++ NewLife.Holiday/HolidayExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace NewLife.Holiday;
+
+/// <summary>假期扩展</summary>
+public static class HolidayExtensions
+{
+    /// <summary>中国假期</summary>
+    public static IHoliday China { get; set; } = new ChinaHoliday();
+
+    /// <summary>是否中国假期</summary>
+    /// <param name="date"></param>
+    /// <returns></returns>
+    public static Boolean IsChinaHoliday(this DateTime date)
+    {
+        var inf = China.Query(date);
+        if (inf != null)
+        {
+            switch (inf.Status)
+            {
+                case HolidayStatus.Normal:
+                    break;
+                case HolidayStatus.On:
+                    return true;
+                case HolidayStatus.Off:
+                    return false;
+                default:
+                    break;
+            }
+        }
+
+        return date.DayOfWeek is DayOfWeek.Saturday or DayOfWeek.Sunday;
+    }
+}
